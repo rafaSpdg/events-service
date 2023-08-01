ef70205de00d43afd15a471a544edf727b39e472
Rafael Gonçalves <spdgrafael@gmail.com>
2023-08-01 16:08:29 +0100

    package changes

---
+++ src/main/java/com/dolphinevents/eventservice/EventRepository.java
@@ -0,0 +1,7 @@
+package com.dolphinevents.eventservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer>{
+    
+}
---
+++ src/main/java/com/dolphinevents/eventservice/EventService.java
@@ -0,0 +1,33 @@
+package com.dolphinevents.eventservice;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    private EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+
+    public List<Event> findAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event findById(Integer id) throws EventNotFoundException {
+         return eventRepository.findById(id).orElseThrow(() -> new EventNotFoundException("Could not find an event in the database."));
+    }
+
+    public void deleteById(Integer id) {
+        eventRepository.deleteById(id);
+    }
+
+    public Event saveEvent(Event event) {
+        eventRepository.save(event);
+        return event;
+    }
+}
